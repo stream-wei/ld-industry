72713b5df5af4f3b79ec8f4bd0126ce163115467
stream <weixuerensoft@gmail.com>
2016-06-09 21:59:40 +0800

    footer

---
+++ php/footer.php
@@ -0,0 +1,55 @@
+<?php ?>
+<div id="footer">
+    <div class="wrap p-re">
+        <div class="f-nav">
+            <dl>
+                <dt>Product</dt>
+                <dd>
+                    <li><a href="http://www.haosteelgroup.com/products.asp?/1.html">Stainless steel bar</a></li>
+                    <li><a href="http://www.haosteelgroup.com/products.asp?/2.html">Stainless steel pipe</a></li>
+                    <li><a href="http://www.haosteelgroup.com/products.asp?/3.html">Stainless steel sheet</a></li>
+                    <li><a href="http://www.haosteelgroup.com/products.asp?/4.html">Stainless steel wire</a></li>
+                </dd>
+            </dl>
+            <dl>
+                <dt>About</dt>
+                <dd>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/1a1.html">About us</a></li>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/2a1.html">Certificate</a></li>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/3a1.html">Team Workers</a></li>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Service</dt>
+                <dd>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/4a2.html">Process Flow</a></li>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/5a2.html">Packing</a></li>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/6a2.html">FAQS</a></li>
+                </dd>
+            </dl>
+            <dl>
+                <dt>Culture</dt>
+                <dd>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/7a3.html">Principle</a></li>
+                    <li><a href="http://www.haosteelgroup.com/information.asp?/8a3.html">Philosophy</a></li>
+                </dd>
+            </dl>
+        </div>
+        <div class="f-link">
+            <img src="./footer_files/erweima.png" class="ewm">
+            <dl>
+                <dt>FIND US SOCIAL MEDIA</dt>
+                <dd class="clearfix">
+                    <a href="http://www.haosteelgroup.com/#"><img src="./footer_files/icon1.png"></a>
+                    <a href="http://www.haosteelgroup.com/#"><img src="./footer_files/icon2.png"></a>
+                    <a href="http://www.haosteelgroup.com/#"><img src="./footer_files/icon3.png"></a>
+                    <a href="http://www.haosteelgroup.com/#"><img src="./footer_files/icon4.png"></a>
+                    <a href="http://www.haosteelgroup.com/#"><img src="./footer_files/icon5.png"></a>
+                    <a href="http://www.haosteelgroup.com/#"><img src="./footer_files/icon6.png"></a></dd>
+            </dl>
+        </div>
+        <div class="clearboth"></div>
+        <div class="f-copy">Copyright © Haosteel Group Co., Limited All Rights Reserved.
+            <a href="http://www.haosteelgroup.com/sitemap.asp">Sitemap</a></div>
+    </div>
+</div>
--- php/index.php
+++ php/index.php
@@ -1,5 +1,64 @@
 <?php
-    include_once '../html/header.html';
-
-    include_once '../html/footer.html';
+include_once '../html/header.html';
+?>
+    <div id="floor_1_main">
+        <div id="floor_1">
+            <div class="left_title">
+                <h2 class="h2_2"><a href="about.php" title="ABOUT US">ABOUT US</a></h2></div>
+            <div class="left_body">
+                <div class="ChannelSContent1">
+                    <a href="about.php" title="ABOUT US"><img src="../resources/images/index/c1.jpg" alt="关于我们"></a>
+                    <p>某某照明始于1996年，是一家集研发、生产和销售于一体的照明企业。历经18年的发展，公司现有员工6000 多人，拥有上海总部及工业园多个生产基地<br>公司产品涵盖LED及传统光源、灯具、电工电器、吊顶产品等领域。<br>
+                        历经18年的发展，公司现有员工6000 多人，拥有上海总部及中山工业园、吴江工业园等多个生产基地；
+                        对销售渠道建设，建立全国性的专卖店体系。<br>
+                        立足于照明市场特点及用户购买习惯，强大的营销队伍和完善的国内外营销网络。各类渠道终端销售网点超 过3000家。<br></p></div>
+            </div>
+            <div class="left_bottom" style="margin-top: -17px;">
+                <a class="ViewDetail" href="about.php" title="ABOUT US"><h2>DETAILS</h2></a>
+            </div>
+        </div>
+    </div>
+    <div id="floor_2_main">
+        <div id="floor_2">
+            <div class="left_title1"><h2><a href="product.php" target="_blank" title="产品中心">产品中心分类</a>
+                </h2></div>
+            <div class="left_body1">
+                <ul class="gridlist2"><!--循环开始-->
+                    <li class="li1"><a class="img" href="http://0133.wangzhan31.com/snzm.html" title="室内照明" target="_blank"><img
+                                src="../resources/images/index/4.jpg" title="室内照明" alt="室内照明"></a><a class="ChannelName"
+                                                                                                     href="http://0133.wangzhan31.com/snzm.html"
+                                                                                                     title="室内照明" target="_blank">室内照明</a>
+                    </li>
+                    <li class="li2"><a class="img" href="http://0133.wangzhan31.com/dgcp.html" title="电工产品" target="_blank"><img
+                                src="../resources/images/index/2(1).jpg" title="电工产品" alt="电工产品"></a><a class="ChannelName"
+                                                                                                        href="http://0133.wangzhan31.com/dgcp.html"
+                                                                                                        title="电工产品" target="_blank">电工产品</a>
+                    </li>
+                    <li class="li3"><a class="img" href="http://0133.wangzhan31.com/gyzm.html" title="光源照明" target="_blank"><img
+                                src="../resources/images/index/3.jpg" title="光源照明" alt="光源照明"></a><a class="ChannelName"
+                                                                                                     href="http://0133.wangzhan31.com/gyzm.html"
+                                                                                                     title="光源照明" target="_blank">光源照明</a>
+                    </li>
+                    <li class="li4"><a class="img" href="http://0133.wangzhan31.com/hwzm.html" title="户外照明" target="_blank"><img
+                                src="../resources/images/index/6.jpg" title="户外照明" alt="户外照明"></a><a class="ChannelName"
+                                                                                                     href="http://0133.wangzhan31.com/hwzm.html"
+                                                                                                     title="户外照明" target="_blank">户外照明</a>
+                    </li>
+                    <li class="li5"><a class="img" href="http://0133.wangzhan31.com/zycp.html" title="专业产品" target="_blank"><img
+                                src="../resources/images/index/1(1).jpg" title="专业产品" alt="专业产品"></a><a class="ChannelName"
+                                                                                                        href="http://0133.wangzhan31.com/zycp.html"
+                                                                                                        title="专业产品" target="_blank">专业产品</a>
+                    </li>
+                    <li class="li6"><a class="img" href="http://0133.wangzhan31.com/jcdd.html" title="集成吊顶" target="_blank"><img
+                                src="../resources/images/index/5.jpg" title="集成吊顶" alt="集成吊顶"></a><a class="ChannelName"
+                                                                                                     href="http://0133.wangzhan31.com/jcdd.html"
+                                                                                                     title="集成吊顶" target="_blank">集成吊顶</a>
+                    </li><!--循环结束--></ul>
+            </div>
+            <div class="left_bottom1"><a class="ChannelName" href="product.php"
+                                         title="产品中心"></a></div>
+        </div>
+    </div>
+<?php
+include_once '../html/footer.html';
 ?>
