11bce1a5d34bd63519f5072b5b020df50bcf7c61
stream <weixuerensoft@gmail.com>
2016-06-10 20:04:36 +0800

    index end

--- php/index.php
+++ php/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../html/header.html';
+include_once '../php/header.php';
 ?>
     <div id="floor_1_main">
         <div id="floor_1">
@@ -7,21 +7,22 @@ include_once '../html/header.html';
                 <h2 class="h2_2"><a href="about.php" title="ABOUT US">ABOUT US</a></h2></div>
             <div class="left_body">
                 <div class="ChannelSContent1">
-                    <a href="about.php" title="ABOUT US"><img src="../resources/images/index/c1.jpg" alt="关于我们"></a>
+                    <a href="about.php" title="ABOUT US"><!--<img src="../resources/images/index/c1.jpg" alt="关于我们">--></a>
                     <p>某某照明始于1996年，是一家集研发、生产和销售于一体的照明企业。历经18年的发展，公司现有员工6000 多人，拥有上海总部及工业园多个生产基地<br>公司产品涵盖LED及传统光源、灯具、电工电器、吊顶产品等领域。<br>
                         历经18年的发展，公司现有员工6000 多人，拥有上海总部及中山工业园、吴江工业园等多个生产基地；
                         对销售渠道建设，建立全国性的专卖店体系。<br>
                         立足于照明市场特点及用户购买习惯，强大的营销队伍和完善的国内外营销网络。各类渠道终端销售网点超 过3000家。<br></p></div>
             </div>
-            <div class="left_bottom" style="margin-top: -17px;">
+            <!--<div class="left_bottom" style="margin-top: -17px;">
                 <a class="ViewDetail" href="about.php" title="ABOUT US"><h2>DETAILS</h2></a>
-            </div>
+            </div>-->
         </div>
     </div>
-    <div id="floor_2_main">
+    <div id="floor_2_main" style="margin-top: -40px;">
         <div id="floor_2">
-            <div class="left_title1"><h2><a href="product.php" target="_blank" title="产品中心">产品中心分类</a>
-                </h2></div>
+            <div class="left_title1">
+                <h2><a href="product.php" target="_blank" title="产品中心">PRODUCTS</a></h2>
+            </div>
             <div class="left_body1">
                 <ul class="gridlist2"><!--循环开始-->
                     <li class="li1"><a class="img" href="http://0133.wangzhan31.com/snzm.html" title="室内照明" target="_blank"><img
@@ -55,10 +56,133 @@ include_once '../html/header.html';
                                                                                                      title="集成吊顶" target="_blank">集成吊顶</a>
                     </li><!--循环结束--></ul>
             </div>
-            <div class="left_bottom1"><a class="ChannelName" href="product.php"
-                                         title="产品中心"></a></div>
+            <!--<div class="left_bottom1"><a class="ChannelName" href="product.php"
+                                         title="产品中心"></a></div>-->
+        </div>
+    </div>
+    <div id="floor_3_main">
+        <div id="floor_3">
+            <div id="floor_3_1">
+                <div class="left_title2"><h2><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">公司新闻</a>
+                    </h2><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻"><img src="../resources/images/index/1(2).jpg"
+                                                                                                 title="公司新闻"></a></div>
+                <div class="left_body2">
+                    <ul class="textlist1">
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">[公司新闻]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/companynews/187.html" target="_blank"
+                                title="普通照明增加 LED市场未来将获大发展">普通照明增加 LED市场未来将获大发展</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">[公司新闻]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/companynews/186.html" target="_blank"
+                                title="LED显示屏行业竞争现状分析">LED显示屏行业竞争现状分析</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">[公司新闻]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/companynews/185.html" target="_blank"
+                                title="居然之家涉足灯饰专业卖场">居然之家涉足灯饰专业卖场</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">[公司新闻]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/companynews/184.html" target="_blank"
+                                title="LED灯具市场仍需整治">LED灯具市场仍需整治</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">[公司新闻]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/companynews/177.html" target="_blank"
+                                title="灯饰选择六原则教您如何选好灯">灯饰选择六原则教您如何选好灯</a><span class="Time">2015-09-24</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">[公司新闻]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/companynews/176.html" target="_blank"
+                                title="八大问题“诠释”LED照明跨境电商“百态”">八大问题“诠释”LED照明跨境电商“百态”</a><span class="Time">2015-09-24</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="left_bottom2"><a class="more" href="http://0133.wangzhan31.com/companynews.html" title="公司新闻">更多..</a>
+                </div>
+            </div>
+            <div id="floor_3_2">
+                <div class="left_title2"><h2><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">行业动态</a>
+                    </h2><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态"><img src="../resources/images/index/2(2).jpg"
+                                                                                                    title="行业动态"></a></div>
+                <div class="left_body2">
+                    <ul class="textlist1">
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">[行业动态]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/industrytrends/191.html" target="_blank"
+                                title="电子行业:下半年LED照明启动,龙头公司受益">电子行业:下半年LED照明启动,龙头公司受益</a><span
+                                class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">[行业动态]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/industrytrends/190.html" target="_blank"
+                                title="前景诱人 智能照明打造新增长点">前景诱人 智能照明打造新增长点</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">[行业动态]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/industrytrends/189.html" target="_blank"
+                                title="2013年国内灯具市场：尽吹复古风？">2013年国内灯具市场：尽吹复古风？</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">[行业动态]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/industrytrends/188.html" target="_blank"
+                                title="价格战如火如荼 LED球泡销量爆发">价格战如火如荼 LED球泡销量爆发</a><span class="Time">2015-09-25</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">[行业动态]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/industrytrends/183.html" target="_blank"
+                                title="选购既节能又能装饰的灯具">选购既节能又能装饰的灯具</a><span class="Time">2015-09-24</span></li>
+                        <li><span class="ChannelName"><a href="http://0133.wangzhan31.com/industrytrends.html" title="行业动态">[行业动态]</a></span><a
+                                class="InfoSTitle" href="http://0133.wangzhan31.com/industrytrends/182.html" target="_blank"
+                                title="灯具安装的注意事项">灯具安装的注意事项</a><span class="Time">2015-09-24</span></li>
+                    </ul>
+                </div>
+                <div class="left_bottom2"><a class="more" href="http://0133.wangzhan31.com/industrytrends.html"
+                                             title="行业动态">更多..</a></div>
+            </div>
+        </div>
+    </div>
+    <div id="floor_4_main">
+        <div id="floor_4">
+            <div class="left_title4"><h2><a href="http://0133.wangzhan31.com/product.html" target="_blank"
+                                            title="产品中心">PARTNER</a></h2></div>
+            <div class="left_body4">
+                <div class="gridlist4">
+                    <div class="bd">
+                        <div id="footer-slider">
+                            <ul class="gridlist"><!--循环开始-->
+                                <li><a href="http://0133.wangzhan31.com/tyfg/166.html" target="_blank"><img
+                                            src="../resources/images/index/22.jpg" title="田园5" alt="田园5"></a><a class="InfoSTitle"
+                                                                                                                href="http://0133.wangzhan31.com/tyfg/166.html"
+                                                                                                                target="_blank" title="田园5">田园5</a>
+                                </li>
+                                <li><a href="http://0133.wangzhan31.com/tyfg/165.html" target="_blank"><img
+                                            src="../resources/images/index/21.jpg" title="田园4" alt="田园4"></a><a class="InfoSTitle"
+                                                                                                                href="http://0133.wangzhan31.com/tyfg/165.html"
+                                                                                                                target="_blank" title="田园4">田园4</a>
+                                </li>
+                                <li><a href="http://0133.wangzhan31.com/tyfg/164.html" target="_blank"><img
+                                            src="../resources/images/index/25.jpg" title="田园3" alt="田园3"></a><a class="InfoSTitle"
+                                                                                                                href="http://0133.wangzhan31.com/tyfg/164.html"
+                                                                                                                target="_blank" title="田园3">田园3</a>
+                                </li>
+                                <li><a href="http://0133.wangzhan31.com/tyfg/163.html" target="_blank"><img
+                                            src="../resources/images/index/24.jpg" title="田园2" alt="田园2"></a><a class="InfoSTitle"
+                                                                                                                href="http://0133.wangzhan31.com/tyfg/163.html"
+                                                                                                                target="_blank" title="田园2">田园2</a>
+                                </li>
+                                <li><a href="http://0133.wangzhan31.com/tyfg/162.html" target="_blank"><img
+                                            src="../resources/images/index/23.jpg" title="田园1" alt="田园1"></a><a class="InfoSTitle"
+                                                                                                                href="http://0133.wangzhan31.com/tyfg/162.html"
+                                                                                                                target="_blank" title="田园1">田园1</a>
+                                </li>
+                                <li><a href="http://0133.wangzhan31.com/tyfg/162.html" target="_blank"><img
+                                            src="../resources/images/index/23.jpg" title="田园1" alt="田园1"></a><a class="InfoSTitle"
+                                                                                                                href="http://0133.wangzhan31.com/tyfg/162.html"
+                                                                                                                target="_blank" title="田园1">田园1</a>
+                                </li>
+
+                                <!--循环结束--></ul>
+                        </div>
+                    </div>
+                </div>
+                <script>
+                    jQuery(".gridlist4").slide({
+                        mainCell: '#footer-slider ul',
+                        autoPlay: true,
+                        autoPage: true,
+                        delayTime: 500,
+                        effect: 'left',
+                        vis:5
+                    });
+                </script>
+            </div>
+            <!--<div class="left_bottom4"><a class="ChannelName" href="http://0133.wangzhan31.com/product.html"
+                                         title="产品中心"></a></div>-->
         </div>
     </div>
 <?php
-include_once '../html/footer.html';
+include_once '../php/footer.php';
 ?>
